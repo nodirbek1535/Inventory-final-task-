fbab3668fab2e09d0ff6afcec40322cec9b9ed92
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-05-03 15:04:14 +0500

    DATA: Add InventoryTag Model

---
+++ Models/InventoryTags/InventoryTag.cs
@@ -0,0 +1,21 @@
+﻿//======================================
+//Nodirbek Nasrullayev Inventory Project
+//======================================
+
+
+using Inventory_final_task_.Models.Inventories;
+using Inventory_final_task_.Models.Tags;
+
+namespace Inventory_final_task_.Models.InventoryTags
+{
+    public class InventoryTag
+    {
+        public Guid Guid { get; set; }
+
+        public Guid InventoryId { get; set; }
+        public Inventory Inventory { get; set; }
+
+        public Guid TagId { get; set; }
+        public Tag Tag { get; set; }
+    }
+}
