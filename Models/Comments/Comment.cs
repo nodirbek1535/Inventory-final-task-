98e9adc93239d3bea6fc4fab995115750c3085c2
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-05-03 17:52:27 +0500

    DATA: Add Comment Model

---
+++ Models/Comments/Comment.cs
@@ -0,0 +1,19 @@
+﻿//======================================
+//Nodirbek Nasrullayev Inventory Project
+//======================================
+
+
+using Inventory_final_task_.Models.Users;
+
+namespace Inventory_final_task_.Models.Comments
+{
+    public class Comment
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+        public Guid TargetId { get; set; }
+        public Guid CreatedByUserId { get; set; }
+        public User CreatedByUser { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+}
