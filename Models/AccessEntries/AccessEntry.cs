b51ed532a7b153b1dacfa668d1a8c66ac00fe119
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-05-03 18:09:42 +0500

    DATA: Add AccessEntry Model

---
+++ Models/AccessEntries/AccessEntry.cs
@@ -0,0 +1,18 @@
+﻿//======================================
+//Nodirbek Nasrullayev Inventory Project
+//======================================
+
+
+namespace Inventory_final_task_.Models.AccessEntries
+{
+    public class AccessEntry
+    {
+        public Guid Id { get; set; }
+        public Guid InventoryId { get; set; }
+        public Inventory Inventory { get; set; }
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+        public AccessLevel AccessLevel { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+}
