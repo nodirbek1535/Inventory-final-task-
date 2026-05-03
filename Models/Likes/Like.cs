1fc71df22346d0e5a33bcef46358a66fd96dec5c
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-05-03 17:59:53 +0500

    DATA: Add Like Model

---
+++ Models/Likes/Like.cs
@@ -0,0 +1,18 @@
+﻿//======================================
+//Nodirbek Nasrullayev Inventory Project
+//======================================
+
+
+using Inventory_final_task_.Models.Users;
+
+namespace Inventory_final_task_.Models.Likes
+{
+    public class Like
+    {
+        public Guid Id { get; set; }
+        public Guid TargetId { get; set; }
+        public Guid UserId{ get; set; }
+        public User User { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+}
