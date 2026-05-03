787404fddff9eab0d23027498cc80f773a81c64d
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-05-03 14:41:17 +0500

    DATA: Add IdElements Model

---
+++ Models/IdElements/IdElement.cs
@@ -0,0 +1,20 @@
+﻿//======================================
+//Nodirbek Nasrullayev Inventory Project
+//======================================
+
+
+using Inventory_final_task_.Models.Inventories;
+
+namespace Inventory_final_task_.Models.IdElements
+{
+    public class IdElement
+    {
+        public Guid Guid { get; set; }
+        public Guid InventoryId { get; set; }
+        public Inventory Inventory { get; set; }
+        public string ElementType { get; set; }
+        public string FixedTextVsalue { get; set; }
+        public string FormatOptions { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
