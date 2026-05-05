402ee07f00484c1dd95553c04d52f6594869f713
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-05-05 21:51:13 +0500

    BROKERS: Add StorgeBroker

---
+++ Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,30 @@
+﻿//======================================
+//Nodirbek Nasrullayev Inventory Project
+//======================================
+
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory_final_task_.Brokers.Storages
+{
+    public partial class StorageBroker : DbContext, IStorageBroker
+    {
+        private readonly IConfiguration configuration;
+
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = 
+                this.configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+
+        public override void Dispose() { }
+    }
+}
