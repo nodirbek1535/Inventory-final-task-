b7acea61fc058dded74316f06fef3d6e0690758e
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-05-07 22:02:23 +0500

    BROKERS: Add CRUD Methods For User

---
+++ Brokers/Storages/IStorageBroker.Users.cs
@@ -0,0 +1,18 @@
+﻿//======================================
+//Nodirbek Nasrullayev Inventory Project
+//======================================
+
+
+using Inventory_final_task_.Models.Users;
+
+namespace Inventory_final_task_.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<User> InsertUserAsync(User user);
+        IQueryable<User> SelectAllUsers();
+        ValueTask<User> SelectUserByIdAsync(Guid userId);
+        ValueTask<User> UpdateUserAsync(User user);
+        ValueTask<User> DeleteUserAsync(User user);
+    }
+}
---
+++ Brokers/Storages/StorageBroker.Users.cs
@@ -0,0 +1,45 @@
+﻿//======================================
+//Nodirbek Nasrullayev Inventory Project
+//======================================
+
+
+using Inventory_final_task_.Models.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory_final_task_.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<User> Users { get; set; }
+
+        public async ValueTask<User> InsertUserAsync(User user)
+        {
+            this.Users.Add(user);
+            await this.SaveChangesAsync();
+
+            return user;
+        }
+
+        IQueryable<User> IStorageBroker.SelectAllUsers() =>
+            this.SelectAll<User>();
+
+        public async ValueTask<User> SelectUserByIdAsync(Guid userId) =>
+            await this.Users.FindAsync(userId);
+
+        public async ValueTask<User> UpdateUserAsync(User user)
+        {
+            this.Users.Update(user);
+            await this.SaveChangesAsync();
+
+            return user;
+        }
+
+        public async ValueTask<User> DeleteUserAsync(User user)
+        {
+            this.Users.Remove(user);
+            await this.SaveChangesAsync();
+
+            return user;
+        }
+    }
+}
