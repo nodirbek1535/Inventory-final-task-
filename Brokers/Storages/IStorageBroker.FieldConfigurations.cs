1c7a25552ac0b87db5b4e2a737352a835110db9b
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-05-07 22:29:29 +0500

    BROKERS: Add CRUD Methods For FieldConfigurations

---
+++ Brokers/Storages/IStorageBroker.FieldConfigurations.cs
@@ -0,0 +1,18 @@
+﻿//======================================
+//Nodirbek Nasrullayev Inventory Project
+//======================================
+
+
+using Inventory_final_task_.Models.FieldConfigurations;
+
+namespace Inventory_final_task_.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<FieldConfiguration> InsertFieldConfigurationAsync(FieldConfiguration fieldConfiguration);
+        IQueryable<FieldConfiguration> SelectAllFieldConfigurations();
+        ValueTask<FieldConfiguration> SelectFieldConfigurationByIdAsync(Guid fieldConfigurationId);
+        ValueTask<FieldConfiguration> UpdateFieldConfigurationAsync(FieldConfiguration fieldConfiguration);
+        ValueTask<FieldConfiguration> DeleteFieldConfigurationAsync(FieldConfiguration fieldConfiguration);
+    }
+}
---
+++ Brokers/Storages/StorageBroker.FieldConfigurations.cs
@@ -0,0 +1,45 @@
+﻿//======================================
+//Nodirbek Nasrullayev Inventory Project
+//======================================
+
+
+using Inventory_final_task_.Models.FieldConfigurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory_final_task_.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<FieldConfiguration> FieldConfigurations { get; set; }
+
+        public async ValueTask<FieldConfiguration> InsertFieldConfigurationAsync(FieldConfiguration fieldConfiguration)
+        {
+            this.FieldConfigurations.Add(fieldConfiguration);
+            await this.SaveChangesAsync();
+
+            return fieldConfiguration;
+        }
+
+        public IQueryable<FieldConfiguration> SelectAllFieldConfigurations() =>
+            this.SelectAll<FieldConfiguration>();
+
+        public async ValueTask<FieldConfiguration> SelectFieldConfigurationByIdAsync(Guid fieldConfigurationId) =>
+            await this.FieldConfigurations.FindAsync(fieldConfigurationId); 
+
+        public async ValueTask<FieldConfiguration> UpdateFieldConfigurationAsync(FieldConfiguration fieldConfiguration)
+        {
+            this.FieldConfigurations.Update(fieldConfiguration);
+            await this.SaveChangesAsync();
+
+            return fieldConfiguration;
+        }
+
+        public async ValueTask<FieldConfiguration> DeleteFieldConfigurationAsync(FieldConfiguration fieldConfiguration)
+        {
+            this.FieldConfigurations.Remove(fieldConfiguration);
+            await this.SaveChangesAsync();
+
+            return fieldConfiguration;
+        }
+    }
+}
