37c2a94ba486d1a2f0a60f503fbac36022eadb50
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-05-07 21:30:58 +0500

    BROKERS: Add CRUD Methods For Inventory Model

---
+++ Brokers/Storages/IStorageBroker.Inventories.cs
@@ -0,0 +1,18 @@
+﻿//======================================
+//Nodirbek Nasrullayev Inventory Project
+//======================================
+
+
+using Inventory_final_task_.Models.Inventories;
+
+namespace Inventory_final_task_.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<Inventory> InsertInventoryAsync(Inventory inventory);
+        IQueryable<Inventory> SelectAllInventories();
+        ValueTask<Inventory> SelectInventoryByIdAsync(Guid inventoryId);
+        ValueTask<Inventory> UpdateInventoryAsync(Inventory inventory);
+        ValueTask<Inventory> DeleteInventoryAsync(Inventory inventory);
+    }
+}
---
+++ Brokers/Storages/StorageBroker.Inventories.cs
@@ -0,0 +1,53 @@
+﻿//======================================
+//Nodirbek Nasrullayev Inventory Project
+//======================================
+
+
+using Inventory_final_task_.Models.Inventories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory_final_task_.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Inventory> Inventories { get; set; }
+
+        public async ValueTask<Inventory> InsertInventoryAsync(Inventory inventory)
+        {
+            using var broker = new StorageBroker(this.configuration);
+            broker.Inventories.Add(inventory);
+            await broker.SaveChangesAsync();
+
+            return inventory;
+        }
+
+        public IQueryable<Inventory> SelectAllInventories() =>
+            this.SelectAll<Inventory>();
+
+        public async ValueTask<Inventory> SelectInventoryByIdAsync(Guid inventoryId)
+        {
+            using var broker = new StorageBroker(this.configuration);
+
+            return await broker.Inventories
+                .FirstOrDefaultAsync(inv => inv.Id == inventoryId);
+        }
+
+        public async ValueTask<Inventory> UpdateInventoryAsync(Inventory inventory)
+        {
+            using var broker = new StorageBroker(this.configuration);
+            broker.Inventories.Update(inventory);
+            await broker.SaveChangesAsync();
+
+            return inventory;
+        }
+
+        public async ValueTask<Inventory> DeleteInventoryAsync(Inventory inventory)
+        {
+            using var broker = new StorageBroker(this.configuration);
+            broker.Inventories.Remove(inventory);
+            await broker.SaveChangesAsync();
+
+            return inventory;
+        }
+    }
+}
