d65f9a630e3d4787a78de8fc2092b02044f11eb8
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-05-11 20:20:42 +0500

    BROKERS: Add CRUD Methods For IdElements

---
+++ Brokers/Storages/IStorageBroker.IdElement.cs
@@ -0,0 +1,18 @@
+﻿//======================================
+//Nodirbek Nasrullayev Inventory Project
+//======================================
+
+
+using Inventory_final_task_.Models.IdElements;
+
+namespace Inventory_final_task_.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<IdElement> InsertIdElementAsync(IdElement idElement);
+        IQueryable<IdElement> SelectAllIdElements();
+        ValueTask<IdElement> SelectIdElementByIdAsync(Guid idElementId);
+        ValueTask<IdElement> UpdateIdElementAsync(IdElement idElement);
+        ValueTask<IdElement> DeleteIdElementAsync(IdElement idElement);
+    }
+}
---
+++ Brokers/Storages/StorageBroker.IdElements.cs
@@ -0,0 +1,45 @@
+﻿//======================================
+//Nodirbek Nasrullayev Inventory Project
+//======================================
+
+
+using Inventory_final_task_.Models.IdElements;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory_final_task_.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<IdElement> IdElements { get; set; }
+
+        public async ValueTask<IdElement> InsertIdElementAsync(IdElement idElement)
+        {
+            this.IdElements.Add(idElement);
+            await this.SaveChangesAsync();
+
+            return idElement;
+        }
+
+        public IQueryable<IdElement> SelectAllIdElements() =>
+            this.SelectAll<IdElement>();
+
+        public async ValueTask<IdElement> SelectIdElementByIdAsync(Guid idElementId) =>
+            await this.IdElements.FindAsync(idElementId);
+
+        public async ValueTask<IdElement> UpdateIdElementAsync(IdElement idElement)
+        {
+            this.IdElements.Update(idElement);
+            await this.SaveChangesAsync();
+
+            return idElement;
+        }
+
+        public async ValueTask<IdElement> DeleteIdElementAsync(IdElement idElement)
+        {
+            this.IdElements.Remove(idElement);
+            await this.SaveChangesAsync();
+
+            return idElement;
+        }
+    }
+}
